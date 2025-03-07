0cbb4e6ecfefb8e5a06212f4d7819eff7f86325d
franziweindel <franziska.weindel@tum.de>
2025-03-07 09:20:05 +0000

    Reorganized project structure

--- implementation/ChachingFace/download_Starcoder.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-checkpoint = "bigcode/starcoder2-15b"
-
-cache_dir = "/franziska/implementation/ChachingFace"
-
-# Load the tokenizer and model from the custom cache directory
-tokenizer = AutoTokenizer.from_pretrained(checkpoint, cache_dir=cache_dir)
-model = AutoModelForCausalLM.from_pretrained(checkpoint, cache_dir=cache_dir)
